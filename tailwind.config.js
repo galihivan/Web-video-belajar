477ca1ddd1849187b201647714a369ffa6c17325
galihivan <galihivan815@gmail.com>
2024-11-02 14:17:21 +0700

    web video learning
    video belajar adalah platform pembelajaran video berbasis web

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'custom-cream': '#FFFDF3',  'custom-green': '#3ECF4C',
+      },
+    },
+  },
+  plugins: [],
+}
